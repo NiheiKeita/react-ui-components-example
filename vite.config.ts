f56b644f01b040f626e74613fdd435d1f24bbc67
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-07-10 21:49:46 +0900

    色々修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ["react", "tailwindcss"],
+      output: {
+        globals: {
+          react: "React",
+          tailwindcss: "tailwindcss",
+        },
+      },
+    },
+  },
+});
